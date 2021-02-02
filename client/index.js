06ddbae5509cd5697cf21bfe75d9c0c6bdbabb5c
gruntbatch <gruntbatch@gmail.com>
2021-02-02 13:49:53 -0800

    Create a mockup of the panel's design

---
+++ client/index.js
@@ -0,0 +1,69 @@
+/** Copyright (C) 2020  Peter Gregory */
+
+// Establish a line of communication with the host
+var csInterface = new CSInterface();
+var extensionId = csInterface.getExtensionID();
+csInterface.addEventListener(
+    "com.adobe.PhotoshopJSONCallback" + extensionId,
+    photoshopEventCallback);
+
+var eventExch = 1165517672; // "Exch"
+var eventRset = 1383294324; // "Rset"
+var eventSetd = 1936028772; // "setd"
+register(eventExch);
+register(eventSetd);
+register(eventRset);
+
+// Create client graphics
+
+function photoshopEventCallback(event) {
+    // updatePanel();
+}
+
+function onClickEventCallback(event) {
+    // dataset = event.currentTarget.dataset;
+    // setForegroundColor(dataset.h, dataset.s, dataset.b);
+}
+
+function register(eventId) {
+    var event = new CSEvent("com.adobe.PhotoshopRegisterEvent", "APPLICATION");
+    event.extensionId = extensionId;
+    event.data = eventId.toString();
+    csInterface.dispatchEvent(event);
+}
+
+function setForegroundColor(h, s, b) {
+    csInterface.evalScript(`setForegroundHSB(${h}, ${s}, ${b})`);
+}
+
+function createPanel() {
+    createSaturation();
+    createPalette();
+}
+
+// https://stackoverflow.com/a/31851617
+function hsb_to_hsl(h, s, v) {
+    s = s / 100;
+    v = v / 100;
+    // both hsv and hsl values are in [0, 1]
+    var l = (2 - s) * v / 2;
+
+    if (l != 0) {
+        if (l == 1) {
+            s = 0
+        } else if (l < 0.5) {
+            s = s * v / (l * 2)
+        } else {
+            s = s * v / (2 - l * 2)
+        }
+    }
+
+    return [h, s * 100, l * 100]
+}
+
+// Is it easier to find a clamp function on so than to write your own?
+// Of course it is.
+// https://stackoverflow.com/a/11410079
+function clamp(num, min, max) {
+    return num <= min ? min : num >= max ? max : num;
+}
---
+++ host/index.jsx
@@ -0,0 +1,12 @@
+/** Copyright (C) 2020  Peter Gregory */
+
+function getForegroundHSB() {
+    var hsb = app.foregroundColor.hsb;
+    return ["[", hsb.hue, ", ", hsb.saturation, ", ", hsb.brightness, "]"].join("");
+}
+
+function setForegroundHSB(h, s, b) {
+    app.foregroundColor.hsb.hue = h;
+    app.foregroundColor.hsb.saturation = s;
+    app.foregroundColor.hsb.brightness = b;
+}
